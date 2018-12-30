a6c0831c562e6a17eaeba39f4e773be6c001a255
Soorena Soleimani <s.soorena@gmail.com>
2018-12-30 23:36:04 +0330

    Add new dist

--- dist/css.js
+++ dist/css.js
@@ -65,7 +65,7 @@ var ReactDigraph =
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a7132d8d1644e63b287";
+/******/ 	var hotCurrentHash = "b9287ac7c2f70933e34d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
